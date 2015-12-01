a19d39761d9938940deccdc0f20219be30ddff43
Accelll <owenshotbolt@gmail.com>
2015-12-01 13:47:32 +0000

    Update home.php

--- home.php
+++ home.php
@@ -1,15 +1,7 @@
 <?php
 session_start();
 include 'includes/dbconnect.php';
-if(!isset($_SESSION['user']))
-{
-	header("Location: login.php");
-}
-$res=mysqli_query($conn,"SELECT Email FROM users WHERE user_id=".$_SESSION['user']);
-while($row = mysqli_fetch_array($res))
-{
-    $_SESSION['username'] = $row['Email'];
-}
+include 'includes/home.php';
 ?>
 
 
